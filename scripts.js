8cb29fe0a8b6470ec502274bbe6d150aa3551b7f
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2019-01-10 12:01:42 +0200

    Add lightbox

--- scripts.js
+++ scripts.js
@@ -3,17 +3,22 @@ var images = document.getElementsByClassName('image');
 var lightboxes = document.getElementsByClassName('lightbox');
 
 var ebay_lightbox = document.getElementById('ebay-lightbox');
-
 var ebay_image = document.getElementById('ebay-image');
-
 var ebay_previous = document.getElementById('ebay-previous');
 var ebay_next = document.getElementById('ebay-next');
-
 var ebay_lightbox_images = ebay_lightbox.getElementsByClassName("lightbox-image");
+
+var recipe_lightbox = document.getElementById('recipe-lightbox');
+var recipe_image = document.getElementById('recipe-image');
+var recipe_previous = document.getElementById('recipe-previous');
+var recipe_next = document.getElementById('recipe-next');
+var recipe_lightbox_images = recipe_lightbox.getElementsByClassName("lightbox-image");
+
+var exit_buttons = document.getElementsByClassName('exit');
 
 const show_image = (images, prev, next) => index => {
     for(var x = 0; x < images.length; x++) {
-        if (x === index % images.length) {
+        if (x === Math.abs(index % images.length)) {
             images[x].style.display = 'block';
         } else {
             images[x].style.display = 'none';
@@ -23,25 +28,38 @@ const show_image = (images, prev, next) => index => {
     }
 }
 
+const show_image_2 = (images, prev, next) => index => {
+    for(var x = 0; x < images.length; x++) {
+        if (x === Math.abs(index % images.length)) {
+            images[x].style.display = 'block';
+        } else {
+            images[x].style.display = 'none';
+        }
+        prev.onclick = () => recipe_carousel(index - 1);
+        next.onclick = () => recipe_carousel(index + 1);
+    }
+}
+
 const ebay_carousel = show_image(ebay_lightbox_images, ebay_previous, ebay_next);
 
 ebay_image.onclick = () => {
     ebay_lightbox.style.display = 'block';
     ebay_carousel(0);
-    // ebay_next.onclick = () => {
-    //     ebay_carousel(1);
-    // }
-    // ebay_carousel(1).style.display = 'block';
-    // ebay_lightbox_images[0].style.display = "block";
 }
 
+const recipe_carousel = show_image_2(recipe_lightbox_images, recipe_previous, recipe_next);
 
-window.onclick = function(event) {
+recipe_image.onclick = () => {
+    recipe_lightbox.style.display = 'block';
+    recipe_carousel(0);
+}
 
-    if (event.target.getAttribute("class") === 'lightbox') {
-        for(var x = 0; x < lightboxes.length; x++) {
-            lightboxes[x].style.display = "none";
-        }
-        // lightbox.style.display = "none";
+for (var x = 0; x < exit_buttons.length; x++) {
+    exit_buttons[x].onclick = () => closeLightboxes();
+}
+
+const closeLightboxes = () => {
+    for(var x = 0; x < lightboxes.length; x++) {
+        lightboxes[x].style.display = "none";
     }
-} 
+}
